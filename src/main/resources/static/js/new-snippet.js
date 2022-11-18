106d5ac498e43272a655f058046b66b15fb0eb14
Emad Ali <mc.lilzee@hotmail.com>
2022-11-18 22:38:28 +0100

    Adjust form to send json request

--- src/main/resources/static/js/new-snippet.js
+++ src/main/resources/static/js/new-snippet.js
@@ -1,4 +1,25 @@
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
-})
+    const codeValue = e.target.elements.code.value;
+    e.preventDefault();
+    const json = JSON.stringify(getCodeObject(codeValue));
+    postJsonRequest(json);
+});
+
+function getCodeObject(value) {
+    return {
+        code: value
+    }
+}
+
+function postJsonRequest(json) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "/api/code/new", false);
+    xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    xhr.send(json);
+
+    if (xhr.status == 200) {
+        alert("success!");
+    }
+}
