9065e5ea95589756dde78e076b96c8cc1af723cb
Emad Ali <mc.lilzee@hotmail.com>
2022-11-18 16:35:45 +0100

    Add creating snippet template

--- src/main/java/platform/presentation/HtmlController.java
+++ src/main/java/platform/presentation/HtmlController.java
@@ -1,9 +1,11 @@
 package platform.presentation;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import platform.business.CodeInformation;
 
 import javax.validation.Valid;
