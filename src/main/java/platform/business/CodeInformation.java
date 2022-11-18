f6d938bd56059134312c88147fff1051e0891730
Emad Ali <mc.lilzee@hotmail.com>
2022-11-18 16:01:24 +0100

    Add date to objects

---
+++ src/main/java/platform/business/CodeInformation.java
@@ -0,0 +1,44 @@
+package platform.business;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+public class CodeInformation {
+
+    private final String code;
+
+    private final LocalDateTime date;
+
+    public CodeInformation(String code) {
+        this.code = code;
+        this.date = LocalDateTime.now();
+    }
+
+    public String getDate() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        return this.date.format(formatter);
+    }
+
+    @JsonIgnore
+    public String getHtml() {
+        return """
+                <html>
+                    <header>
+                        <title>Code</title>
+                    </header>
+                    <body>
+                        <pre>
+                            %s
+                        </pre>
+                    </body>
+                </html>
+                """.formatted(this.code);
+    }
+
+
+
+}
--- src/main/java/platform/presentation/HtmlController.java
+++ src/main/java/platform/presentation/HtmlController.java
@@ -3,18 +3,23 @@ package platform.presentation;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import platform.business.CodePlatform;
+import platform.business.CodeInformation;
 
 @RestController
 public class HtmlController {
 
-    CodePlatform platform = new CodePlatform("""
+    CodeInformation platform = new CodeInformation("""
             public static void main(String[] args) {
                 SpringApplication.run(CodeSharingPlatform.class, args);
             }""");
 
     @GetMapping(value = "/code", produces = MediaType.TEXT_HTML_VALUE)
-    public String getCode() {
+    public String getHtml() {
         return platform.getHtml();
+    }
+
+    @GetMapping("/api/code")
+    public CodeInformation getJson() {
+        return platform;
     }
 }
